5093a8222967b432c4b2e1194c625143fa728d78
Ilya Belinsky <belinskyilya@gmail.com>
2026-03-28 15:04:44 +0300

    switchboard methods and tests completed

--- app/switchboard.py
+++ app/switchboard.py
@@ -2,10 +2,14 @@ from __future__ import annotations
 
 from dataclasses import dataclass
 
-from app.users import User
-
+from app.users import User, LocalUser, ForeignUser
 
 LOCAL_PHONE_PREFIX = "+7"
+
+
+class InvalidCallFormatException(Exception):
+    """Raised when the call string format is invalid."""
+    pass
 
 
 @dataclass(slots=True)
@@ -20,7 +24,14 @@ class ActiveCall:
 
 class Switchboard:
     def __init__(self) -> None:
+        self.cross_border_calls: int = 0
         self._active_calls: list[ActiveCall] = []
+
+    def create_user(self, user_id: int, fullname: str, phone: str) -> User:
+        if phone.startswith(LOCAL_PHONE_PREFIX):
+            return LocalUser(user_id, fullname, phone)
+        else:
+            return ForeignUser(user_id, fullname, phone)
 
     def register_call(self, raw_call: str) -> ActiveCall:
         '''
@@ -29,10 +40,39 @@ class Switchboard:
 
         Например: "1001,Иван Петров,+71234567890,1085,Адам Яковлев,+71255556666"
         '''
-        pass  # Удалите `pass` и пишите ваш код
+        user_info = raw_call.split(",")
+        if len(user_info) != 6:
+            raise InvalidCallFormatException(
+                f"Format of raw input is incorrect. Expected 6 field, got {len(list(filter(lambda info: info, user_info)))}"
+                # такой оверхед получился для прохождения теста с пустой строкой в raw_call, иначе было "expected 6, got 1"
+            )
+
+        try:
+            caller_id = int(user_info[0])
+            caller_name = user_info[1]
+            caller_phone = user_info[2]
+            if not caller_phone.startswith("+") or not caller_phone[1:].isdigit():
+                raise InvalidCallFormatException("Incorrect format of phone number")
+            caller = self.create_user(caller_id, caller_name, caller_phone)
+
+            receiver_id = int(user_info[3])
+            receiver_name = user_info[4]
+            receiver_phone = user_info[5]
+            if not receiver_phone.startswith("+") or not receiver_phone[1:].isdigit():
+                raise InvalidCallFormatException("Incorrect format of phone number")
+            receiver = self.create_user(receiver_id, receiver_name, receiver_phone)
+        except ValueError:
+            raise InvalidCallFormatException(f"Incorrect format of caller_id or receiver_id")
+
+        active_call = ActiveCall(caller, receiver)
+        self._active_calls.append(active_call)
+        if active_call.is_cross_border:
+            self.cross_border_calls += 1
+        return active_call
 
     def get_active_calls_count(self) -> int:
-        pass  # Удалите `pass` и пишите ваш код
+        active_calls = self._active_calls
+        return len(active_calls)
 
     def get_cross_border_calls_count(self) -> int:
-        pass  # Удалите `pass` и пишите ваш код
+        return self.cross_border_calls
--- tests/test_switchboard.py
+++ tests/test_switchboard.py
@@ -1,6 +1,6 @@
 import pytest
 
-from app.switchboard import Switchboard
+from app.switchboard import Switchboard, InvalidCallFormatException
 from app.users import ForeignUser, LocalUser
 
 
@@ -30,6 +30,11 @@ def test_register_call_counts_active_calls() -> None:
     assert switchboard.get_active_calls_count() == 2
 
 
+def test_register_call_counts_active_calls_should_return_zero() -> None:  # возможно этот тест уже лишний, добавил для обработки крайнего случая
+    switchboard = Switchboard()
+    assert switchboard.get_active_calls_count() == 0
+
+
 def test_register_call_counts_calls_between_local_and_foreign_users() -> None:
     switchboard = Switchboard()
 
@@ -45,3 +50,72 @@ def test_register_call_counts_calls_between_local_and_foreign_users() -> None:
 
     assert switchboard.get_active_calls_count() == 3
     assert switchboard.get_cross_border_calls_count() == 1
+
+
+def test_register_call_counts_calls_between_local_and_foreign_users_should_return_zero() -> None:  # тоже ради крайнего случая
+    switchboard = Switchboard()
+
+    switchboard.register_call(
+        "3,Petr Petrov,+78880000000,4,Maria Petrova,+79991112233"
+    )
+    switchboard.register_call(
+        "5,Jane Doe,+73123456789,6,Alex Doe,+742012345678"
+    )
+
+    assert switchboard.get_cross_border_calls_count() == 0
+
+
+def test_register_call_with_incorrect_format_of_raw_call_should_raise_exception() -> None:
+    switchboard = Switchboard()
+    with pytest.raises(InvalidCallFormatException, match="Format of raw input is incorrect. Expected 6 field, got 0"):
+        switchboard.register_call("")
+    with pytest.raises(InvalidCallFormatException, match="Format of raw input is incorrect. Expected 6 field, got 3"):
+        switchboard.register_call("123,123,123")
+
+
+def test_register_call_with_incorrect_userid_in_raw_call_should_raise_exception() -> None:
+    switchboard = Switchboard()
+    with pytest.raises(InvalidCallFormatException, match="Incorrect format of caller_id or receiver_id"):
+        switchboard.register_call(
+            "ERROR,Jane Doe,+33123456789,TEST,Alex Doe,+442012345678"
+        )
+    with pytest.raises(InvalidCallFormatException, match="Incorrect format of caller_id or receiver_id"):
+        switchboard.register_call(
+            "123,Jane Doe,+33123456789,NO,Alex Doe,+442012345678"
+        )
+    with pytest.raises(InvalidCallFormatException, match="Incorrect format of caller_id or receiver_id"):
+        switchboard.register_call(
+            "NO,Jane Doe,+33123456789,234,Alex Doe,+442012345678"
+        )
+
+
+def test_register_call_phone_number_doesnt_start_with_plus_should_raise_exception() -> None:
+    switchboard = Switchboard()
+    with pytest.raises(InvalidCallFormatException, match="Incorrect format of phone number"):
+        switchboard.register_call(
+            "1,Ivan Ivanov,79990000000,2,John Smith,15551234567"
+        )
+    with pytest.raises(InvalidCallFormatException, match="Incorrect format of phone number"):
+        switchboard.register_call(
+            "1,Ivan Ivanov,+59990000000,2,John Smith,15551234567"
+        )
+    with pytest.raises(InvalidCallFormatException, match="Incorrect format of phone number"):
+        switchboard.register_call(
+            "1,Ivan Ivanov,59990000000,2,John Smith,+15551234567"
+        )
+
+
+def test_register_call_phone_number_contains_letters_should_raise_exception() -> None:
+    switchboard = Switchboard()
+    with pytest.raises(InvalidCallFormatException, match="Incorrect format of phone number"):
+        switchboard.register_call(
+            "1,Ivan Ivanov,+7AAA5404000,2,John Smith,+7BBB51234567"
+        )
+    with pytest.raises(InvalidCallFormatException, match="Incorrect format of phone number"):
+        switchboard.register_call(
+            "1,Ivan Ivanov,+7AAA5404000,2,John Smith,+77651234567"
+        )
+    with pytest.raises(InvalidCallFormatException, match="Incorrect format of phone number"):
+        switchboard.register_call(
+            "1,Ivan Ivanov,+76545404000,2,John Smith,+7BBB51234567"
+        )
